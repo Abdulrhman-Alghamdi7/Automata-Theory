f5a69636d3fb4f2a264b78750cd7ee534d96cf0b
Abdulrhman Alghamdi <96546956+Abdulrhman-Alghamdi7@users.noreply.github.com>
2025-09-30 03:02:41 +0300

    Refactor FiniteAutomata and add MultiInput classes

--- Automata.py
+++ Automata.py
@@ -23,7 +23,7 @@ class State:
         yield self
 class FiniteAutomata:
     id = 1
-    def __init__(self,Q:set, Σ:set, δ:dict[tuple[State, Union[str,Symbol]], Union[set,State]],q:State, F:set[State]):
+    def __init__(self,Q:set, Σ:set[str], δ:dict[tuple[State, Union[str,Symbol]], Union[set,State]],q:State, F:set[State]):
         self.Q = self.states = Q
         self.Σ = self.alphabet =  Σ
         self.δ = self.transition_function = δ
@@ -62,7 +62,7 @@ class FiniteAutomata:
             for q in r:
                 r1 = r1.union(self.E(self.δ.get((q,i))))
             r = r1
-            print(f"({i},{r})")
+            print(f"({i}, {r})")
         for q in r:
             if q in self.F:
                 print(f'M{self.ID} accepts {w}')
@@ -74,14 +74,13 @@ class FiniteAutomata:
         stack = [NFAstart]
         NFA_state_to_DFA = {frozenset(NFAstart):State()}
         q = NFA_state_to_DFA[frozenset(NFAstart)]
-        δ:dict[tuple[State, str], State] = dict()
+        δ:dict[tuple[State, Union[str,Symbol]], State] = dict()
         F = set()
         while stack:
             NFAstate = stack.pop()
             DFAstate = NFA_state_to_DFA[frozenset(NFAstate)]
-            for i in NFAstate:
-                if i in self.F:
-                    F.add(DFAstate)
+            if any(q in self.F for q in NFAstate):
+                F.add(DFAstate)
             for s in self.Σ:
                 new = set()
                 for NFAq in NFAstate:
@@ -230,3 +229,133 @@ class RegularLanguages:
             elif parse_tree[0] == star:M = FiniteAutomata.star(RegularLanguages.regexToNFA(parse_tree[1]))
             elif parse_tree[0] == union:M = FiniteAutomata.union(RegularLanguages.regexToNFA(parse_tree[1]),RegularLanguages.regexToNFA(parse_tree[2]))
         return M
+
+class MultiInputFiniteAutomata:
+    id = 1
+    def __init__(self,Q:set, Σ:set[tuple[str]], δ:dict[tuple[State, tuple[Union[str,Symbol]]], Union[set,State]],q:State, F:set[State], n:int) -> None:    
+        self.Q = self.states = Q
+        self.Σ = self.alphabet =  Σ
+        self.δ = self.transition_function = δ
+        self.q = self.start_state = q 
+        self.F = self.final_states = F
+        self.n = self.input_strings_number = n
+        self.ID = FiniteAutomata.id
+        FiniteAutomata.id += 1
+    def __repr__(self):
+        return f"M{self.ID}=(Q={self.Q},\nΣ={self.Σ},\nδ={self.δ},\nq={self.q},\nF={self.F},\nn={self.n})"
+    def E(self,R:Union[set[State], State]) -> set[State]:
+        if R is None:return set()
+        if isinstance(R,set):
+            closure = set(R)
+            stack = list(R)
+        else:
+            closure = {R}
+            stack = [R]
+        while stack:
+            state = stack.pop()
+            S = self.δ.get((state,ε))
+            if S is None:continue
+            if isinstance(S,set):
+                for next in S:
+                    if next not in closure:
+                        closure.add(next)
+                        stack.append(next)
+            else:
+                if S not in closure:
+                    closure.add(S)
+                    stack.append(S)
+        return closure
+    def computation(self,*w:str) -> bool:
+        if len(w) != self.n:raise Exception("The number of inputs doesn't equal to the specified number of inputs!")
+        if any(len(w[i]) != len(w[i+1]) for i in range(len(w)-1)):raise Exception("Input strings must have the same length!")
+        r:set = self.E({self.q})
+        for i in range(len(w[0])):
+            t = tuple(s[i] for s in w)
+            r1 = set()
+            for q in r:
+                r1 = r1.union(self.E(self.δ.get((q,t))))
+            r = r1
+            print(f"({t},{r})")
+        for q in r:
+            if q in self.F:
+                print(f'M{self.ID} accepts {w}')
+                return True
+        print(f'M{self.ID} rejects {w}')
+        return False
+    def determinization(self) -> "MultiInputFiniteAutomata":
+        NFAstart = self.E(self.q)
+        stack = [NFAstart]
+        NFA_state_to_DFA = {frozenset(NFAstart):State()}
+        q = NFA_state_to_DFA[frozenset(NFAstart)]
+        δ:dict[tuple[State, tuple[Union[str,Symbol]]], State] = dict()
+        F = set()
+        while stack:
+            NFAstate = stack.pop()
+            DFAstate = NFA_state_to_DFA[frozenset(NFAstate)]
+            if any(q in self.F for q in NFAstate):
+                F.add(DFAstate)
+            for s in self.Σ:
+                new = set()
+                for NFAq in NFAstate:
+                    new = new.union(self.E(self.δ.get((NFAq,s))))
+                newfrozen = frozenset(new)
+                if newfrozen not in NFA_state_to_DFA:
+                    NFA_state_to_DFA[newfrozen] = State()
+                    stack.append(new)
+                δ[(DFAstate, s)] = NFA_state_to_DFA[newfrozen]
+        Q = set(NFA_state_to_DFA.values())
+        Σ = self.Σ
+        return MultiInputFiniteAutomata(Q,Σ,δ,q,F,self.n)
+
+class FiniteStateTransducer:
+    id = 1
+    def __init__(self,Q:set, Σ:set[str], δ:dict[tuple[State, str], tuple[State, str]],q:State, Γ:set[str]) -> None:
+        if len(δ) != len(Q)*len(Σ):raise Exception("A finite state transducer must be deterministic!")
+        if any(t[1] == ε for t in δ):raise Exception("A finite state transducer must be deterministic!")
+        self.Q = self.states = Q
+        self.Σ = self.input_alphabet =  Σ
+        self.δ = self.transition_function = δ
+        self.q = self.start_state = q 
+        self.Γ = self.output_alphabet = Γ
+        self.ID = FiniteAutomata.id
+        FiniteAutomata.id += 1
+    def __repr__(self):
+        return f"M{self.ID}=(Q={self.Q},\nΣ={self.Σ},\nδ={self.δ},\nq={self.q},\nΓ={self.Γ})" 
+    def computation(self,w:str):
+        q = self.q
+        output_string = ''
+        for s in w:
+            if (q,s) not in self.δ:raise Exception(f"No transition defined for ({q},{s})!")
+            q,o = self.δ[(q,s)]
+            output_string+=o
+            print(f"{s}, {q}, {output_string}")
+        return output_string
+    
+class MultiInputFiniteStateTransducer:
+    id = 1
+    def __init__(self,Q:set, Σ:set[tuple[str]], δ:dict[tuple[State, tuple[str]], tuple[State, str]],q:State, Γ:set[str], n:int) -> None:
+        if len(δ) != len(Q)*len(Σ):raise Exception("A finite state transducer must be deterministic!")
+        if any(t[1] == ε for t in δ):raise Exception("A finite state transducer must be deterministic!")
+        self.Q = self.states = Q
+        self.Σ = self.input_alphabet =  Σ
+        self.δ = self.transition_function = δ
+        self.q = self.start_state = q 
+        self.Γ = self.output_alphabet = Γ
+        self.n = self.input_strings_number = n
+        self.ID = FiniteAutomata.id
+        FiniteAutomata.id += 1
+    def __repr__(self):
+        return f"M{self.ID}=(Q={self.Q},\nΣ={self.Σ},\nδ={self.δ},\nq={self.q},\nΓ={self.Γ}\nn={self.n})" 
+    def computation(self,*w:str):
+        if len(w) != self.n:raise Exception("The number of inputs doesn't equal to the specified number of inputs!")
+        if any(len(w[i]) != len(w[i+1]) for i in range(len(w)-1)):raise Exception("Input strings must have the same length!")
+        q = self.q
+        output_string = ''
+        for i in range(len(w[0])):
+            t = tuple(s[i] for s in w)
+            if (q,t) not in self.δ:raise Exception(f"No transition defined for ({q},{t})!")
+            q,o = self.δ[(q,t)]
+            output_string+=o
+            print(f"{t}, {q}, {output_string}")
+        return output_string
+    
